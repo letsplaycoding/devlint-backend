242e3cf22c06efbc5fafe6e32d819fec258ae5c8
DESKTOP-N598GAM\동호 <alu98@naver.com>
2025-12-08 00:55:18 +0900

    Update dependencies, yml

--- src/main/java/com/example/devlintBE/DevlintApplication.java
+++ src/main/java/com/example/devlintBE/DevlintApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.devlintBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DevlintApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DevlintApplication.class, args);
 	}
 
 }
